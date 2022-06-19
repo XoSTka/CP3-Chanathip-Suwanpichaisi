de26836d7f4e0e01d5ad4e6bad7eda63e7e2d563
XoSTka <cpchampion12@gmail.com>
2022-06-19 23:17:41 +0700

    Create Exercise4_Chanathip_S.py

---
+++ Exercise4_Chanathip_S.py
@@ -0,0 +1,9 @@
+FE = 81.2
+GB = 89.7
+ICS = 93.7
+CP = 99.0
+print("--- Your Score ---")
+print("Foundation English :", FE)
+print("General Business :", GB)
+print("Introduction to Computer Systems :", ICS)
+print("Computer Programming :", CP)
