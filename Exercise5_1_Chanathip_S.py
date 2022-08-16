a04276f250ba26ad74038e7efba3f8e908b9dbdf
XoSTka <106858590+XoSTka@users.noreply.github.com>
2022-08-16 23:05:09 +0700

    Create Exercise5_1_Chanathip_S.py

---
+++ Exercise5_1_Chanathip_S.py
@@ -0,0 +1,13 @@
+print("!---Number must be equal or more than 0---!")
+num1 = int(input("Fill first number : "))
+num2 = int(input("Fill second number : "))
+
+plus = num1 + num2
+minus = num1 - num2
+multiply = num1 * num2
+divide = num1 / num2
+
+print(num1,"+",num2,"=",plus)
+print(num1,"-",num2,"=",minus)
+print(num1,"*",num2,"=",multiply)
+print(num1,"/",num2,"=",divide)
