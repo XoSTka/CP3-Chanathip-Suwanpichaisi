1ed37fc8948d74f4878ff0cc36e260a5f211e79f
XoSTka <cpchampion12@gmail.com>
2023-04-05 00:40:50 +0700

    Create Lecture50_Chanathip_S.py

---
+++ Lecture50_Chanathip_S.py
@@ -0,0 +1,12 @@
+def plusNumber(x,y):
+    print(x+y)
+def minusNumber(x,y):
+    print(x-y)
+def multiplyNumber(x,y):
+    print(x*y)
+def divideNumber(x,y):
+    print(x/y)
+plusNumber(420,69)
+minusNumber(420,69)
+multiplyNumber(420,69)
+divideNumber(420,69)
