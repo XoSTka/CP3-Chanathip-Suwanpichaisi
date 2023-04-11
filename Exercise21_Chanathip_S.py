3ac79d6c2c4109707dc1e6966f9d3c57383b2b9b
XoSTka <cpchampion12@gmail.com>
2023-04-12 00:43:52 +0700

    Create Exercise21_Chanathip_S.py

---
+++ Exercise21_Chanathip_S.py
@@ -0,0 +1,35 @@
+from tkinter import *
+import math
+
+def leftClickButton(event):
+    print(float(textBoxWeight.get()) / math.pow(float(textBoxHeight.get())/100, 2))
+    BMI = float(textBoxWeight.get()) / math.pow(float(textBoxHeight.get()) / 100, 2)
+    if BMI >= 30.0:
+        labelResult.configure(text="อ้วนมาก")
+    elif 25 <= BMI < 30.0:
+        labelResult.configure(text="อ้วน")
+    elif 23 <= BMI < 25.0:
+        labelResult.configure(text="น้ำหนักเกิน")
+    elif 18.6 <= BMI < 23.0:
+        labelResult.configure(text="น้ำหนักปกติ")
+    elif BMI < 18.6:
+        labelResult.configure(text="ผอมเกินไป")
+    else:
+        labelResult.configure(text="Error")
+
+MainWindow = Tk()
+labelHeight = Label(MainWindow, text="ส่วนสูง (cm.)")
+labelHeight.grid(row=0, column=0)
+textBoxHeight = Entry(MainWindow)
+textBoxHeight.grid(row=0, column=1)
+labelWeight = Label(MainWindow, text="น้ำหนัก (Kg.)")
+labelWeight.grid(row=1, column=0)
+textBoxWeight = Entry(MainWindow)
+textBoxWeight.grid(row=1, column=1)
+calculateButton = Button(MainWindow, text="คำนวณ")
+calculateButton.bind("<Button-1>", leftClickButton)
+calculateButton.grid(row=2, column=0)
+labelResult = Label(MainWindow, text="ผลลัพธ์")
+labelResult.grid(row=2, column=1)
+
+MainWindow.mainloop()
